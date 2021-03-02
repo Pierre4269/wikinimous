5f97e8af9ce9b79573b3556a7edb07902f0e68f3
pierre thomas <pierre.thomas@essca.eu>
2021-03-02 17:08:39 +0100

    fini

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "suppression des articles"
+
+Article.destroy_all
+
+puts "Creation articles"
+
+10.times do
+  article = Article.new(
+    title: Faker::Book.title,
+    content: Faker::Lorem.characters
+  )
+  article.save!
+end
